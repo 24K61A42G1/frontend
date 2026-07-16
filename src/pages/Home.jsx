c9e6353e1d5c981255aea2408c2ffe48f4e562d6
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-07-16 17:16:14 +0530

    commit 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,19 @@ import MovieCard from "../components/MovieCard";
 function Home() {
   const [movies, setMovies] = useState([]);
 
+  const normalizeMovies = (data) => {
+    if (Array.isArray(data)) return data;
+    if (data && Array.isArray(data.movies)) return data.movies;
+    if (data && typeof data === "object" && (data._id || data.title || data.poster)) {
+      return [data];
+    }
+    return [];
+  };
+
   useEffect(() => {
     axios
       .get("https://backend-0mp3.onrender.com/api/movies")
-      .then((res) => setMovies(res.data))
+      .then((res) => setMovies(normalizeMovies(res.data)))
       .catch((err) => console.log(err));
   }, []);
 
